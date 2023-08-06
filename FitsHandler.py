df836d319442509f63fc1bfd5b0e80af47fc3325
hannah <hananeh.mt@gmail.com>
2023-08-06 15:56:09 +0330

    add focal length and pixel size to FitsHandler attributes add calculate_flux_of_region method add calculate_mean_pixel_flux method add find_darkest_area_method add get_pixel_scale_by_arcsecond method

--- FitsHandler.py
+++ FitsHandler.py
@@ -1,6 +1,7 @@
 import numpy as np
 from astropy.io import fits
 from matplotlib import pyplot as plt
+from numpy import ndarray
 
 
 def plot_file(image: np.ndarray):
@@ -12,13 +13,15 @@ def plot_file(image: np.ndarray):
 
 class FitsHandler:
 
-    def __init__(self, fits_path: str):
+    def __init__(self, fits_path: str, focal_length: int, pixel_size: float):
         self.fits_path: str = fits_path
         print("reading fits file...")
         hdu_list = fits.open(self.fits_path)
         fits_image = hdu_list[0].data
         hdu_list.close()
         self.fits_image: np.ndarray = fits_image
+        self.focal_length: int = focal_length
+        self.pixel_size: float = pixel_size
 
     def get_file(self) -> np.ndarray:
         return self.fits_image
@@ -40,3 +43,38 @@ class FitsHandler:
         plt.title('Histogram of the Region')
         plt.grid(True)
         plt.show()
+
+    def calculate_flux_of_region(self, region) -> float:
+        x1, y1, x2, y2 = region
+        sum_flux = 0
+        for i in range(x1, x2):
+            for j in range(y1, y2):
+                sum_flux += self.fits_image[i, j]
+        return sum_flux
+
+    def calculate_mean_pixel_flux(self, region) -> ndarray:
+        x1, y1, x2, y2 = region
+        region_data = self.fits_image[x1:x2, y1:y2]
+        return np.mean(region_data)
+
+    def find_darkest_area(self, size):
+        height, width = self.fits_image.shape
+        darkest_area_x = 0
+        darkest_area_y = 0
+        min_average_value = np.inf
+
+        for y in range(height - size + 1):
+            for x in range(width - size + 1):
+                window_data = self.fits_image[y:y + size, x:x + size]
+                average_value = np.mean(window_data)
+
+                # Update the darkest area if a new minimum average value is found
+                if average_value < min_average_value:
+                    min_average_value = average_value
+                    darkest_area_x = x
+                    darkest_area_y = y
+
+        return darkest_area_x, darkest_area_y, min_average_value
+
+    def get_pixel_scale_by_arcsecond(self) -> float:
+        return (206265*self.pixel_size)/self.focal_length
