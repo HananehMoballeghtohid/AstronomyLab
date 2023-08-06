71476a99c1b3641227eb588caa40a53abec869e8
hannah <hananeh.mt@gmail.com>
2023-08-06 15:56:52 +0330

    complete code and print results

--- basic-background-mag.py
+++ basic-background-mag.py
@@ -1,19 +1,41 @@
 import os
 
+import numpy as np
+
 from FitsHandler import FitsHandler
 
 
 def main():
-    # Assuming we have already selected regions with no stars,
-    # We draw histograms of our selected regions to see which one is better:
+    # importing fits file:
+    fits_path = os.getcwd() + "/data/stars.fits"
 
-    FITS_PATH = os.getcwd() + "/data/stars.fits"
-    fits_file = FitsHandler(FITS_PATH)
-    region = [475, 575, 650, 750]
+    # 1m telescope and canon 1200D camera:
+    focal_length = 1
+    pixel_size = 4.30 * 1e-6
+    fits_file = FitsHandler(fits_path, focal_length, pixel_size)
+
+    # finding the darkest area and its mean pixel flux:
+    darkest_area_x, darkest_area_y, pixel_mean_flux = fits_file.find_darkest_area(100)
+    print('The darkest area in the file starts at: ', darkest_area_x, ', ', darkest_area_y)
+    print('The average pixel flux in the area is: ', pixel_mean_flux)
+
+    # plotting histogram:
+    region = [darkest_area_x, darkest_area_y, darkest_area_x + 100, darkest_area_y + 100]
     fits_file.plot_histogram_of_region(region)
 
-    # then we need to calculate the total flux of the background
-    # and calculate magnitude comparing to the star
+    # calculating pixel scale
+    pixel_scale = fits_file.get_pixel_scale_by_arcsecond()
+    print("Pixel scale: {:.2f} arcseconds/pixel".format(pixel_scale))
+
+    # calculating flux by arcseconds squared
+    arc_square_mean_flux = pixel_mean_flux / (pixel_scale ** 2)
+    print("mean flux in one arcseconds squared: {:.2f} flux/arcsecond^2".format(arc_square_mean_flux))
+
+    # calculating magnitude
+    reference_mag = 8.88
+    reference_flux = 28535.57
+    magnitude = reference_mag - 2.5 * np.log10(arc_square_mean_flux / reference_flux)
+    print("magnitude of background sky: {:.2f} mag/arcsecond^2".format(magnitude))
 
 
 if __name__ == '__main__':
